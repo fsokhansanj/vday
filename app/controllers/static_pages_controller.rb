ce895d583304ed626ee7868f9aeb8fa73a317c1b
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-01-14 16:36:10 -0800

    created a working version with KJC's help.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,11 @@
 class StaticPagesController < ApplicationController
   def home
+  	if params[:answer]
+  	@string = StringManipulator.new(params[:answer])
+  	@string.array_counter
+  	@how_many_fucks = @string.calculate_fucks
   end
+end
 
   def help
   end
