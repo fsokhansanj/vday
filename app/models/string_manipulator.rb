6f898f2fedd25790b4162ba3ed2f9affd08233bf
Kyle Sponheim <kyle.sponheim@gmail.com>
2016-01-14 16:19:46 -0800

    updated string_manipulator

--- app/models/string_manipulator.rb
+++ app/models/string_manipulator.rb
@@ -1,27 +1,28 @@
 class StringManipulator
-	
-	attr_accessor :text, :array_of_words, :fucks_given
+
+	attr_accessor :user_submitted_string, :array_of_words, :fucks_given
 
 	def initialize(string)
-		@text = string
+		@user_submitted_string = string
 		@fucks_given = 0
 		text_to_array
+		@counts = Array.new
+		@key_words = ["job", "girlfriend", "car", "loan"]
 	end
 
 	def text_to_array
-		self.array_of_words = text.split(/\s+/).map(&:downcase)
+		self.array_of_words = user_submitted_string.split(/\s+/).map(&:downcase)
 	end
 
-	def calculate_fucks_given
-		return self.fucks_given = 0 if array_of_words.any?{ |word| word =~ /job/i }
-		
-		array_of_words.each do |word|
-			case word
-			when 'girlfriend'
-				self.fucks_given += 1
-			when 'garden'
-				self.fucks_given += 1
-			end
+	def array_counter
+		self.array_of_words.each do |check|
+			if @key_words.include?(check)
+			@counts	<< check
 		end
+	end
+end
+
+	def calculate_fucks
+		return @counts.count
 	end
 end
