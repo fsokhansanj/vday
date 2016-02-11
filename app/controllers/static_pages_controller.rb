6feff433d3103ce5944307f9fdb01f52a70a36bb
Farz Sokhansanj <fsokhansanj@gmail.com>
2016-02-11 13:53:55 -0800

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,6 @@ class StaticPagesController < ApplicationController
     	@response = @string.show_answer
     else
       @response = "Name the dating app we used to meet each other"
-  end
   def home
   	if params[:answer]
     	@string = StringManipulator.new(params[:answer])
