232b9a089a29fe6b53d9364af2e80fee64d5362e
Farz Sokhansanj <fsokhansanj@gmail.com>
2016-02-11 13:57:12 -0800

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,6 +5,8 @@ class StaticPagesController < ApplicationController
     	@string.array_counter
     	@response = @string.show_answer
     else
+      @first_question = "What is the name of the dating app we met on?"
+      @second_question = "What is my greatest fear?"
   end
 end
 
