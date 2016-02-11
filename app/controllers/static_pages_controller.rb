909f284fd4ed8c69a9e6caeb44c93dc6305aab32
Farz Sokhansanj <fsokhansanj@gmail.com>
2016-02-11 13:49:31 -0800

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,15 @@ class StaticPagesController < ApplicationController
     else
       @response = "Name the dating app we used to meet each other"
   end
+class StaticPagesController < ApplicationController
+  def home
+  	if params[:answer]
+    	@string = StringManipulator.new(params[:answer])
+    	@string.array_counter
+    	@response = @string.show_answer
+    else
+      @response = "What is my greatest fear?"
+  end
 end
 
   def help
